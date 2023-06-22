c957a65840f2266f1cd243a62b342cbadb3f39ea
Andrii Hutnyk <andreigutnik@gmail.com>
2023-06-22 11:03:34 +0200

    First commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,48 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector("ul.gallery");
+const gallaryItemMarkup = createGallaryItemMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", gallaryItemMarkup);
+
+galleryContainer.addEventListener("click", ongalleryContainerClick);
+
+// Create the gallary
+  const gallery = new SimpleLightbox(".gallery__item a", {
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+	}); 
+
+function createGallaryItemMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+			return `
+			<li class="gallery__item">
+				<a class="gallery__link" href="${original}">
+				<img class="gallery__image" src="${preview}" alt="${description}"/>
+				</a>
+			</li>
+			`;
+			})
+    .join("");
+}
+
+function ongalleryContainerClick(evt) {
+	// Canceling standard actions
+  evt.preventDefault();
+
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }  
+	
+  // Close and refresh the gallary
+	gallery.on("closed.simplelightbox", () => {
+    gallery.refresh();
+  });  
+}
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,11 @@
+import Player from '@vimeo/player';
 
+const player = new Player("vimeo-player");
+
+// player.on('play', function() {
+//    console.log('played the video!');
+// });
+
+// player.on('timeupdate', function () {
+	
+// });
